cd61ca6a9b3b5970a9d09a032d8542dffebf9481
yellowgh0st <burim.berisa@protonmail.com>
2020-06-07 16:16:00 +0200

    Use webfonts, low width mobile displays

--- components/header.tsx
+++ components/header.tsx
@@ -2,25 +2,24 @@ import * as React from "react";
 import {url} from "../common/defaults"
 import Link from 'next/link'
 
-import less from '../particles/less/header.module.less';
+import header from '../particles/less/header.module.less';
 import grid from '../particles/less/grid.module.less';
 import button from '../particles/less/buttons.module.less';
 import utilities from '../particles/less/utilities.module.less';
 
 const Header: React.FunctionComponent = () => (
     <header className={grid.container}>
-        <div className={`${grid.col4} ${utilities.justifyStart}`} >
+        <div className={`${grid.col4} ${header.brand}`} >
             <img onClick={e => {
                 window.location.href = url;
-            }} className={less.logotype}
+            }} className={header.logotype}
                  src="svg/logotype.svg" alt="Vether - A strictly-scarce Ethereum-based asset." />
         </div>
         <div className={grid.col4}>
         </div>
         <div className={`
             ${grid.col4}
-            ${utilities.justifyEnd}
-            ${utilities.alignCenter}
+            ${header.action}
         `}>
             <Link href="https://vetherasset.org/">
                 <a className={button.menubar}>Open Dapp</a>
